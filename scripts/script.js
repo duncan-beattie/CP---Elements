0b043fcca95ffc9d473d577b1f5025ec0efe0cc2
Duncan Beattie <dbeatt@NUC0078.local>
2016-08-16 10:23:47 +0100

    overlay menu, graphs and accordions

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
 $(function(){
+    $('.nav-toggle').click(function() {
+        $("#overlay").toggleClass('is-open' );
+    })
+    /*
+    setTimeout(function(){ 
+        $('.site-header .nav-toggle').trigger('click')
+    },1000);
+    */
+});
+
+
+
+$(function(){
     // change welcome message based on time of day
     if($('.welcome span').length){
         var when = Math.floor((new Date(Date.now()).getHours() / 6 )),
@@ -21,8 +34,27 @@ $(function(){
         var total = 0
         $.each($('.pf_val'), function(i, el){
             total += parseFloat($(el).text())
-            console.log(parseFloat($(el).text()))
         })
         $('.pf_total').text(total.toFixed(2))
     }
+    if($('.block.has_link').length){
+        $.each($('.block.has_link'), function(i, el){
+            $(el).click(function(){
+                $(el).find('a').trigger("click")
+            })
+        })
+    }
+});
+
+
+// Accordions
+$(function(){
+    $.each($('.accordion > div'), function(i, el){
+        $(el).data('hgt', $(el).height())
+        $(el).toggleClass('is-closed')
+    })
+    $('.accordion').find('.toggle').click(function(){
+        console.log('whatcha');
+        $(this).parent().toggleClass('is-closed')
+    })
 });
